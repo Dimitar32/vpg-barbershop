f06adbe9bf732979c21b30472b1586ffae69932e
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2024-11-04 10:07:52 +0200

    t

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Reservation from './pages/Reservation';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Home from './pages/Home/Home';
+import Reservation from './pages/Reservation/Reservation';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
