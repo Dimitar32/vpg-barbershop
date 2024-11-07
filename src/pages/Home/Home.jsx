f246be8f9c5eb0a659a8a253f284f30ad7a7d94a
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2024-11-07 21:10:05 +0200

    fix background image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -26,26 +26,31 @@ const Home = () => {
   
   return (
   <div className="home">
-    <div className='homewatermarkphoto'>
-      <h1>Добре дошли в нашата бръснарница!</h1>
-      <p>Ние предлагаме качествени услуги за подстригване и оформяне на брада, с внимание към детайла и професионализъм.</p>
-      
-      <div className="cta">
-        <h2>Запазете своя час сега!</h2>
-        <Link to="/reservation">
-          <button className="btn-reservation">Запази час</button>
-        </Link>
-      </div>
-      
-      <section className="services">
-        <h2>Нашите услуги</h2>
-        <ul>
-          <li>Подстригване</li>
-          <li>Оформяне на брада</li>
-          <li>Подстригване с брада</li>
-        </ul>
+    
+      {/* Main background section with overlay text and CTA button */}
+      <section className='homewatermarkphoto'>
+        <div className='insidehomewatermarkphoto'>
+          <h1>Добре дошли в нашата бръснарница!</h1>
+          <p>Ние предлагаме качествени услуги за подстригване и оформяне на брада, с внимание към детайла и професионализъм.</p>
+          
+          <div className="cta">
+            <h2>Запазете своя час сега!</h2>
+            <Link to="/reservation">
+              <button className="btn-reservation">Запази час</button>
+            </Link>
+          </div>
+        </div>
       </section>
-    </div>
+        
+        <section className="services">
+          <h2>Нашите услуги</h2>
+          <ul>
+            <li>Подстригване</li>
+            <li>Оформяне на брада</li>
+            <li>Подстригване с брада</li>
+          </ul>
+        </section>
+      
     {/* Динамична секция "Нашите бръснари" */}
     <section className="barbers">
         <h2>Нашите бръснари</h2>
