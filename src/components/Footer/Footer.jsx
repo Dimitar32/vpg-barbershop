dd457a0b21b022be3253f760f03cc4eded326f24
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2024-11-07 22:38:11 +0200

    fix address

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => (
   <footer className="footer">
     <p>&copy; {new Date().getFullYear()} VPG Barber. Всички права запазени.</p>
-    <p>Адрес: бул. Цар Симеон Велики 122, Град Стара Загора</p>
+    <p>Адрес: бул. Цар Симеон Велики 258, Град Стара Загора</p>
     <p>Телефон: +359 123 456 789</p>
   </footer>
 );
